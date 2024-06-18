4db6bd430c7f37fd75f38542b92f9f783e7518de
Med Ali Khaled <medalikhaled331@gmail.com>
2024-06-18 23:02:59 +0100

    fix: light mode stuff

--- app/web/src/app/page.tsx
+++ app/web/src/app/page.tsx
@@ -29,39 +29,51 @@ export default function HomePage() {
         </div>
       </nav>
 
+      <div className="flex h-48 w-full items-end justify-center px-8 py-4 md:px-20">
+        <h1 className="text-clip bg-gradient-to-br from-indigo-600 to-teal-600 bg-clip-text text-transparent">
+          Placeholder [ANIMTED LOGO]
+        </h1>
+      </div>
+
       <section className="flex w-full flex-1 flex-col items-center justify-center">
         <div className="container flex flex-col items-center justify-center gap-2 rounded-2xl md:w-2/3 md:flex-row md:gap-1">
-          <div className="flex h-12 items-center justify-center gap-1 rounded-md border-0 border-input bg-transparent p-2 shadow-sm transition-colors dark:border-foreground md:w-1/3 md:rounded-l-3xl md:rounded-r-none md:border">
+          <div className="flex h-12 items-center justify-center gap-1 rounded-md border-0 border-foreground bg-transparent p-2 shadow-sm transition-colors md:w-1/3 md:rounded-l-3xl md:rounded-r-none md:border">
             <Button variant="link" size="sm" className="">
               {/*
             opens up a drawer to set more parameter 
             if authed set them to the user, store them to local storage and suggest logging in
             */}
-              <GearIcon className="h-5 w-5 text-indigo-300" />
+              <GearIcon className="h-5 w-5 text-indigo-600 dark:text-indigo-300" />
             </Button>
             <Button
-              variant="secondary"
+              variant="outline"
               size="sm"
-              className="w-full bg-transparent md:dark:bg-indigo-600"
+              className="w-full bg-transparent md:bg-indigo-400 md:dark:bg-indigo-600"
             >
-              <p className="hidden md:inline-block">Upload CV</p>
-              <FilePlusIcon className="h-5 w-5 md:ml-2" />
+              <p className="hidden text-foreground md:inline-block">
+                Upload CV
+              </p>
+              <FilePlusIcon className="h-5 w-5 text-foreground md:ml-2" />
             </Button>
           </div>
           <Input
             type="text"
             placeholder="E.g: Senior DevOps Engineer"
-            className="h-12 rounded-md border-foreground py-4 text-base font-normal transition-colors focus:border-indigo-500 focus:ring-indigo-500 focus:dark:border-indigo-400 focus:dark:ring-indigo-400 md:rounded-l-none md:rounded-r-3xl"
+            className="h-12 rounded-md border-foreground py-4 text-base font-normal transition-colors focus-visible:border-indigo-600 focus-visible:ring-indigo-600 focus:dark:border-indigo-400 focus:dark:ring-indigo-400 md:rounded-l-none md:rounded-r-3xl"
           />
         </div>
 
         <div className="flex w-2/3 items-center justify-center gap-2 py-4">
           <MapPinIcon className="hidden size-4 text-indigo-400" />
 
-          <p className="text-center text-sm italic text-slate-400 md:w-[70ch]">
+          <p className="text-center text-sm italic text-slate-600 dark:text-slate-400 md:w-[70ch]">
             Location set to{" "}
             <span className="text-indigo-400">Sfax, Tunisia</span>. To change,
-            use the <span className="text-indigo-300">gear icon</span>.
+            use the{" "}
+            <span className="text-indigo-400 dark:text-indigo-600">
+              gear icon
+            </span>
+            .
           </p>
         </div>
       </section>
