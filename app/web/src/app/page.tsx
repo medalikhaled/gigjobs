af30559ac84bf177213a41517ba588d0631c50a6
Med Ali Khaled <medalikhaled331@gmail.com>
2024-06-19 17:55:05 +0100

    feat: animations, login page ui,..

--- app/web/src/app/page.tsx
+++ app/web/src/app/page.tsx
@@ -2,9 +2,16 @@
 import Link from "next/link";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
-import { FilePlusIcon, GearIcon, PersonIcon } from "@radix-ui/react-icons";
+import {
+  FilePlusIcon,
+  GearIcon,
+  MoonIcon,
+  PersonIcon,
+} from "@radix-ui/react-icons";
 import { MapPinIcon } from "@heroicons/react/20/solid";
 import { useState } from "react";
+import { BackgroundGradientAnimation } from "~/components/animations/background-gradient-animation";
+import { SparklesCore } from "~/components/animations/sparkles";
 
 export default function HomePage() {
   const [dark, setDark] = useState<boolean>(true);
@@ -14,26 +21,27 @@ export default function HomePage() {
     <main
       className={`${dark && "dark"} flex min-h-screen flex-col items-center bg-background text-foreground`}
     >
-      <nav className="flex w-full items-center justify-between px-8 py-4 md:px-20">
-        <Button variant="default" onClick={() => setDark((prev) => !prev)}>
+      <nav className="z-50 flex w-full items-center justify-between px-8 py-4 md:px-20">
+        <Button variant="default" onClick={() => null}>
           Open sidebar
         </Button>
-        <div>
+
+        <div className="flex items-center justify-center gap-2">
           {authed ? (
             <Link href="/login">Login</Link>
           ) : (
-            <Button variant="ghost" size="icon">
+            <Link href="/login" className="h-6 w-6">
               <PersonIcon className="h-6 w-6" />
-            </Button>
+            </Link>
           )}
+
+          <Button variant="default" onClick={() => setDark((prev) => !prev)}>
+            <MoonIcon className="h-6 w-6" />
+          </Button>
         </div>
       </nav>
 
-      <div className="flex h-48 w-full items-end justify-center px-8 py-4 md:px-20">
-        <h1 className="text-clip bg-gradient-to-br from-indigo-600 to-teal-600 bg-clip-text text-transparent">
-          Placeholder [ANIMTED LOGO]
-        </h1>
-      </div>
+      {/* <HeaderSparkles /> */}
 
       <section className="flex w-full flex-1 flex-col items-center justify-center">
         <div className="container flex flex-col items-center justify-center gap-2 rounded-2xl md:w-2/3 md:flex-row md:gap-1">
@@ -78,5 +86,35 @@ export default function HomePage() {
         </div>
       </section>
     </main>
+  );
+}
+
+export function HeaderSparkles() {
+  return (
+    <div className="flex h-[24rem] w-full flex-col items-center justify-center overflow-hidden rounded-md bg-transparent">
+      <h1 className="relative z-20 text-center text-3xl font-bold text-white md:text-7xl lg:text-9xl">
+        GiG Jobs
+      </h1>
+      <div className="relative h-40 w-[40rem]">
+        {/* Gradients */}
+        <div className="absolute inset-x-20 top-0 h-[2px] w-3/4 bg-gradient-to-r from-transparent via-indigo-500 to-transparent blur-sm" />
+        <div className="absolute inset-x-20 top-0 h-px w-3/4 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
+        <div className="absolute inset-x-60 top-0 h-[5px] w-1/4 bg-gradient-to-r from-transparent via-sky-500 to-transparent blur-sm" />
+        <div className="absolute inset-x-60 top-0 h-px w-1/4 bg-gradient-to-r from-transparent via-sky-500 to-transparent" />
+
+        {/* Core component */}
+        <SparklesCore
+          background="transparent"
+          minSize={0.4}
+          maxSize={1}
+          particleDensity={1200}
+          className="h-full w-full"
+          particleColor="#FFFFFF"
+        />
+
+        {/* Radial Gradient to prevent sharp edges */}
+        <div className="absolute inset-0 h-full w-full bg-transparent [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+      </div>
+    </div>
   );
 }
--- app/web/tailwind.config.ts
+++ app/web/tailwind.config.ts
@@ -59,6 +59,52 @@ const config = {
         sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
+        // backfround gradient
+        moveHorizontal: {
+          "0%": {
+            transform: "translateX(-50%) translateY(-10%)",
+          },
+          "50%": {
+            transform: "translateX(50%) translateY(10%)",
+          },
+          "100%": {
+            transform: "translateX(-50%) translateY(-10%)",
+          },
+        },
+        moveInCircle: {
+          "0%": {
+            transform: "rotate(0deg)",
+          },
+          "50%": {
+            transform: "rotate(180deg)",
+          },
+          "100%": {
+            transform: "rotate(360deg)",
+          },
+        },
+        moveVertical: {
+          "0%": {
+            transform: "translateY(-50%)",
+          },
+          "50%": {
+            transform: "translateY(50%)",
+          },
+          "100%": {
+            transform: "translateY(-50%)",
+          },
+        },
+
+        // metor effect
+        meteor: {
+          "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
+          "70%": { opacity: "1" },
+          "100%": {
+            transform: "rotate(215deg) translateX(-500px)",
+            opacity: "0",
+          },
+        },
+
+        // Shadcn
         "accordion-down": {
           from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
@@ -69,6 +115,17 @@ const config = {
         },
       },
       animation: {
+        // backfround gradient
+        first: "moveVertical 30s ease infinite",
+        second: "moveInCircle 20s reverse infinite",
+        third: "moveInCircle 40s linear infinite",
+        fourth: "moveHorizontal 40s ease infinite",
+        fifth: "moveInCircle 20s ease infinite",
+
+        //metor effect
+        "meteor-effect": "meteor 5s linear infinite",
+
+        // Shadcn
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
